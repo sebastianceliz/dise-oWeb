2c071d11091d6c1f9ee45dfa0900667ef8d969f8
cbas <joelsebitas_10@hotmail.com>
2021-05-26 12:12:10 -0500

    asd

---
+++ sesion16/misScripts.js
@@ -0,0 +1,6 @@
+    $(document).ready(function(){
+        $("a").click(function(evento){
+            alert("Has pulsado el enlace . Ahora seras enviado a la pag de la usat")
+        })
+       
+    })
